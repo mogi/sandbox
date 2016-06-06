23365f5f00db260596f9c0bf357dca9d88edf5a2
Hiroki Mogi <mogi@scaleout.jp>
2016-06-06 13:12:34 +0900

    change method

--- app/root/views.py
+++ app/root/views.py
@@ -14,17 +14,34 @@ class IndexView(TemplateView):
         return ctxt
 
 
+from django.core.urlresolvers import reverse
 class PostTwitterView(RedirectView):
     pattern_name = "index"
+    permanent=False
 
     def get_redirect_url(self, *args, **kwargs):
         article = get_object_or_404(Article, pk=kwargs['message'])
         article.update_counter()
         return super(PostTwitterView, self).get_redirect_url(*args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        url = self.get_redirect_url(*args, **kwargs)
+        if url:
+            if self.permanent:
+                return http.HttpResponsePermanentRedirect(url)
+            else:
+                return http.HttpResponseRedirect(url)
+        else:
+            logger.warning(
+                'Gone: %s', request.path,
+                extra={'status_code': 410, 'request': request}
+            )
+            return http.HttpResponseGone()
+
 
 class AuthTwitterView(RedirectView):
     pattern_name = "index"
+    permanent=False
 
     def get_redirect_url(self, *args, **kwargs):
         return super(PostTwitterView, self).get_redirect_url(*args, **kwargs)
